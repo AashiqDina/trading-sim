bf50acd95b019a309b83c877dc08c7513097262f
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-11 00:52:21 +0000

    Update API endpoint to use production URL

--- src/Functions/getStockApiInfo.tsx
+++ src/Functions/getStockApiInfo.tsx
@@ -3,7 +3,7 @@ import handleTwelveDataError from "../Error/handleTwelveDataError";
 
 export default async function getStockApiInfo(props: any){
     try{
-        const result = await axios.get(`http://localhost:3000/api/stocks/GetStockQuoteInfo/${props.symbol}`)
+        const result = await axios.get(`https://tradingsim-backend.onrender.com/api/stocks/GetStockQuoteInfo/${props.symbol}`)
         console.log(result)
 
         if(result.data.hasError){
@@ -27,4 +27,5 @@ export default async function getStockApiInfo(props: any){
             buttonText: "Retry"})
         return null;
     }
+
 }
