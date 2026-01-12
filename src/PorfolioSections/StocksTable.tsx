85357aa3f362f11c5db971f416945d77db23846a
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-12 00:16:16 +0000

    Update StocksTable.tsx

--- src/PorfolioSections/StocksTable.tsx
+++ src/PorfolioSections/StocksTable.tsx
@@ -233,7 +233,7 @@ function getHoursAgo(date?: string | Date | null): string {
                       <td className="tdLogo"><button aria-label='Visit Stock Details Page' onClick={ () => {navigate(`/stock/${encodeURIComponent(String(stockAvg.symbol ?? ''))}`)}} className='tdLogoButton'><img className="StockLogos" src={stockAvg.logo} alt="Stock Logo" /></button></td>
                       <td className="tdCompanies"><div><div><button aria-label='Expand Stock to see individual stocks bought'><h3>{stockAvg.name}</h3><span>Quantity: {Math.round(stockAvg.totalShares*100)/100}</span></button></div></div></td>
                       <td className="tdBoughtPrice"><div>£{stockAvg.totalCost.toFixed(2)}<span>Average: £{stockAvg.avgBuyPrice.toFixed(2)}</span></div></td>
-                      <td className="tdCurrentValue"><div>£{stockAvg.currentWorth.toFixed(2)}<span className={"LastUpdatedStockTableValue"}>Updated:
+                      <td className="tdCurrentValue"><div>£{stockAvg.currentWorth.toFixed(2)}<span className={"LastUpdatedStockTableValue"}>
                         {LastUpdatedDictionary?.get(stockAvg.symbol)
                           ? getHoursAgo(LastUpdatedDictionary.get(stockAvg.symbol))
                           : "N/A"}
@@ -320,6 +320,7 @@ function getHoursAgo(date?: string | Date | null): string {
     )
 
 }
+
 
 
 
