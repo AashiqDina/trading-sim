a630de28dc627c1be16e6355f2ccd49b8bf49a13
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-12 01:49:29 +0000

    Update StockDetailsOwnedStocks.tsx

--- src/StockDetailsSections/StockDetailsOwnedStocks.tsx
+++ src/StockDetailsSections/StockDetailsOwnedStocks.tsx
@@ -86,8 +86,8 @@ export default function StockDetailsOwnedStocks(props: any){
                      <td className="tdCompanies"><div><div><h3>{stock.name}</h3><span>Quantity: {stock.quantity}</span></div></div></td>
                      <td className="tdBoughtPrice"> £{(stock.purchasePrice*stock.quantity).toFixed(2)}</td>
                       <td className="tdCurrentValue"><div>£{stock.currentPrice.toFixed(2)}<span className={"LastUpdatedStockTableValue"}>
-                        {LastUpdatedDictionary?.get(stock.symbol)
-                          ? getHoursAgo(LastUpdatedDictionary.get(stock.symbol))
+                        {LastUpdatedDictionary?.get(props.symbol)
+                          ? getHoursAgo(LastUpdatedDictionary.get(props.symbol))
                           : "N/A"}
                           </span>
                         </div>
@@ -106,5 +106,6 @@ export default function StockDetailsOwnedStocks(props: any){
     )
 
 }
+
 
 
